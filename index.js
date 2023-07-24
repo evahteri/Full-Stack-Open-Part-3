ed04d33c503bcced8c4952323fa862f819e0e6c7
Eero Vahteri <eero.vahteri@helsinki.fi>
2023-07-24 10:01:25 +0300

    3.5 done

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ app.use(express.json())
 let persons = [
     {
         id: 1,
-        content: "Arto Hellas",
-        important: "040-123456"
+        name: "Arto Hellas",
+        number: "040-123456"
       },
       {
         id: 2,
-        content: "Ada Lovelace",
-        important: "39-44-5323523"
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
       },
       {
         id: 3,
-        content: "Dan Abramov",
-        important: "12-43-234345"
+        name: "Dan Abramov",
+        number: "12-43-234345"
       },
       {
         id: 4,
@@ -61,6 +61,36 @@ app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has info for ${amount} people</p>
     <p>${currentDate}</p>`)
 })
+
+const generateId = () => {
+    const number = Math.floor(Math.random()*1000)
+    return number
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    console.log(body)
+
+    if (!body.name) {
+        return response.status(400).json({ 
+          error: 'content missing' 
+        })
+      }
+
+    const id = generateId()
+    console.log(id)
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: id
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+}
+)
 
 
 const PORT = 3001
